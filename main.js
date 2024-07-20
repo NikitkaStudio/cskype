5de4367418d5c946086b5cf312323b24f0949e0c
NikitkaStudio <nikitoscompany123@xmail.ru>
2024-07-21 00:28:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+const { app, BrowserWindow, nativeImage, Tray } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: 'icon.png'
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+const icon = nativeImage.createFromPath('icon.png')
+tray = new Tray(icon)
+})
+
+const updater = require("electron-updater");
+const autoUpdater = updater.autoUpdater;
+
+autoUpdater.on('checking-for-update', function () {
+    sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', function (info) {
+    sendStatusToWindow('Update available.');
+});
+
+autoUpdater.on('update-not-available', function (info) {
+    sendStatusToWindow('Update not available.');
+});
+
+autoUpdater.on('error', function (err) {
+    sendStatusToWindow('Error in auto-updater.');
+});
+
+autoUpdater.on('download-progress', function (progressObj) {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + parseInt(progressObj.percent) + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+    sendStatusToWindow('Update downloaded; will install in 1 seconds');
+});
+
+autoUpdater.on('update-downloaded', function (info) {
+    setTimeout(function () {
+        autoUpdater.quitAndInstall();
+    }, 1000);
+});
+
+function checkForUpdates(){
+    const data = {
+        'provider': 'github',
+        'owner':    'vkiranmaniya',
+        'repo':     'exchange',
+        'token':    'YOUR_PERSONAL_TOKEN_HERE'
+      };
+    autoUpdater.setFeedURL(data);
+    autoUpdater.checkForUpdates();
+}
+
+function sendStatusToWindow(message) {
+    console.log(message);
+}
+
+module.exports = {
+    checkForUpdates,
+}
+s
